5fb0951b3752e28da77fd041c4451e84846f4bb4
Adarsh Patidar <adarshpatidar789@gmail.com>
2025-10-05 19:22:17 +0530

    Custom Logger Implemented

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # End-to-end project from building data science project with version control and deploying it to aws.
-template.py - this script will be used to automatically create the full project structure.
+Step-1 template.py - this script will be used to automatically create the full project structure.
+Step-2 Custom Logging implementation under src\datascience\__init__.py
--- template.py
+++ template.py
@@ -35,7 +35,7 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory {filedir} for the file : {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0): # if do not exist or exist and empty then create new file/overwrite
         with open(filepath, 'w') as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
